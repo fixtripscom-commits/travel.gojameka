b8c9c36c6300a849d731f492de32a28e464d7e89
fixtripscom-commits <fixtripscom@gmail.com>
2026-04-24 18:18:42 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>GOJAMEKA Travel</h1>
+      <p>Website berhasil live 🚀</p>
+    </div>
+  );
+}
+
+export default App;
